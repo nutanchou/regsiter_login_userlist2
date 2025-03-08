49969d2806b0a0a968754bf62ca961b78c79c01f
nutanchou <nutanchoudhary162502@gmail.com>
2025-03-08 20:31:23 +0530

    Update users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,23 +1,19 @@
-// backend/routes/users.js
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 
-// Registration route
 router.post('/register', async (req, res) => {
   try {
     const { firstName, lastName, email, password } = req.body;
 
-    // Create a new user
     const newUser = new User({ firstName, lastName, email, password });
 
-    // Save the user to the database
     await newUser.save();
 
-    res.status(200).json({ message: 'User registered successfully' });  // Ensure a JSON response
+    res.status(200).json({ message: 'User registered successfully' });
   } catch (err) {
     console.error('Error registering user:', err);
-    res.status(500).json({ message: 'Server Error' });  // Return a JSON error message
+    res.status(500).json({ message: 'Server Error' });
   }
 });
 
