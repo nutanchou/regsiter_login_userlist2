14a81736f0b238bab1aaf6c75e50529628f03138
nutanchou <nutanchoudhary162502@gmail.com>
2025-03-08 20:32:12 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,4 @@
-require('dotenv').config(); // Load environment variables from .env file
+require('dotenv').config();
 const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
