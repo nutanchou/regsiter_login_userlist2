b70eabc520bdd1585e07c717f6603625d36cb62f
nutanchou <nutanchoudhary162502@gmail.com>
2025-03-08 20:30:46 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,36 +1,29 @@
-// backend/routes/authRoutes.js
-
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User'); // Import the User model
+const User = require('../models/User');
 
-// Registration Route
 router.post('/register', async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
 
-  // Validate required fields
   if (!firstName || !lastName || !email || !password) {
     return res.status(400).json({ message: 'All fields are required' });
   }
 
   try {
-    // Check if the user already exists
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: 'User already exists' });
     }
 
-    // Create a new user
     const newUser = new User({
       firstName,
       lastName,
       email,
-      password, // Ensure you hash the password in a real-world app!
+      password,
     });
 
     await newUser.save();
 
-    // Send success response
     res.status(201).json({ message: 'User registered successfully' });
   } catch (err) {
     console.error('Error during registration:', err);
